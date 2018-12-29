6d13254d55b3b84730c393bbbc7cac74a3841fd1
ASchub <ASchub@users.noreply.github.com>
2018-12-30 00:20:24 +0100

    Restart
    Gick tillbaks till en enklare setup, med de väsentliga delarna

--- CprogProjekt/Main.cpp
+++ CprogProjekt/Main.cpp
@@ -1,29 +1,16 @@
 #include <SDL.h>
-#include "Label.h"
 #include "GameEngine.h"
-#include "Button.h"
 #include <string>
+
 using namespace std;
 using namespace cwing;
-
-Label* lbl;
-
-class MyButton : public Button {
-public:
-	MyButton() :Button(50, 100, "Ok") {}
-	void perform(const string& txt) {
-		lbl->setText("Stefan");
-	}
-};
 
 int main(int argc, char** argv) {
 	GameEngine ge;
 
-	lbl = Label::getInstance(100, 100, "Jozef");
-	ge.add(lbl);
 
-	Button* b = new MyButton();
-	ge.add(b);
+
+
 	ge.run();
 	return 0;
 }
